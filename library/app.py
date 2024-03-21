6205dc5fe209d11284cc19302cda263eb66b9fe8
Martinn <martinhruska770@gmail.comgit config --global user.email martinhruska770@gmail.com>
2024-03-21 09:56:05 +0100

    json

--- library/app.py
+++ library/app.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template, request, redirect, url_for
+import json
 
 app = Flask(__name__)
+
+
+def save_to_json(filename, data):
+    with open(filename, 'w') as json_file:
+        json.dump(data, json_file)
+
+def load_from_json(filename):
+    try:
+        with open(filename, 'r') as json_file:
+            return json.load(json_file)
+    except FileNotFoundError:
+        return []
 
 users = []
 books = []
@@ -14,6 +27,7 @@ def add_user():
     if request.method == 'POST':
         user_name = request.form['user_name']
         users.append(user_name)
+        save_to_json('users.json', users) 
         return redirect(url_for('index'))
     return render_template('add_user.html')
 
@@ -22,8 +36,12 @@ def add_book():
     if request.method == 'POST':
         book_title = request.form['book_title']
         books.append(book_title)
+        save_to_json('books.json', books)  
         return redirect(url_for('index'))
     return render_template('add_book.html')
 
 if __name__ == '__main__':
+  
+    users = load_from_json('users.json')
+    books = load_from_json('books.json')
     app.run(debug=True)
